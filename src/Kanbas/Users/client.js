21de6f05cbff4672332f9fe58677e5d3bc3d26f3
Atulya Miglani <atulyamiglani02@gmail.com>
2023-12-04 15:45:52 -0500

    correct env

--- src/Kanbas/Users/client.js
+++ src/Kanbas/Users/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const BASE_API =
-  process.env.REACT_APP_BASE_API_URL || "http://localhost:4000/api";
+  process.env.REACT_APP_API_BASE || "http://localhost:4000/api";
 export const USERS_API = `${BASE_API}/users`;
 const request = axios.create({
   withCredentials: true,
